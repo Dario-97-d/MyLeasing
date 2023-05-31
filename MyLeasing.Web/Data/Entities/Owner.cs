a57855e8039bbbb676249080d49e8668ed032fc9
Dario Dias <dariodias97@gmail.com>
2023-05-31 16:52:07 +0100

    CRUD Owners

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [DisplayName("Document*")]
+        public string Document { get; set; }
+
+        [Required]
+        [DisplayName("First Name*")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name*")]
+        public string LastName { get; set; }
+
+        [DisplayName("Owner Name")]
+        public string OwnerName => $"{FirstName} {LastName}";
+
+        [DisplayName("Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [DisplayName("Cell Phone")]
+        public string CellPhone { get; set; }
+
+        public string Address { get; set; }
+    }
+}
